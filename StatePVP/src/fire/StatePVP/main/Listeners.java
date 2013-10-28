f9d6a80aebeeefba7a0b579bcb699b2255f7736f
Peter <ssssfire52@users.noreply.github.com>
2013-10-28 13:12:44 -0600

    Update Listeners.java

--- StatePVP/src/fire/StatePVP/main/Listeners.java
+++ StatePVP/src/fire/StatePVP/main/Listeners.java
@@ -3,5 +3,9 @@ package fire.StatePVP.main;
 import org.bukkit.event.Listener;
 
 public class Listeners implements Listener {
-
+  
+  @EventHandler
+  public void onRightClick(MouseEvent.RIGHT_CLICK e) {
+    
+  }
 }
